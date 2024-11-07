38281f91f45d7c9c9cb9dfd7a9774aa84bf73055
JonathanDemtroederNachnameBatman <joel.busse@tu-bs.de>
2024-11-07 19:13:21 +0100

    more helpers & rewrite plotting

--- network.py
+++ network.py
@@ -1,4 +1,6 @@
 import random
+from netrc import netrc
+
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
@@ -8,12 +10,13 @@ cmap = ListedColormap([[34, 34, 34], [224, 224, 224]])
 class Network:
 
     # if save is true, plots will be exported. Not useful for jupyter
-    def __init__(self, height, width, probability, save=False):
+    def __init__(self, height, width, probability):
 
         self.height = height
         self.width = width
         self.probability = probability
-        self.save = save
+        self.analysed = False # True if hoshen koppelman was used
+        self.fixed = True # True if all conlficts are fixed in the network
 
         # True, False matrix with occupied spots, hstack and vstack put an unoccupied row/column on the left/top for simpler computation
         self.network = np.hstack((np.ones((height + 1, 1)), np.vstack((np.ones((1, width)), np.random.uniform(0, 1, (height, width)))))) <= probability
@@ -23,8 +26,8 @@ class Network:
         self.N = np.zeros(int(np.ceil(height*width/2)), dtype=np.int32)                # Cluster-Vector containing cluster-sizes. Negative values represent new cluster-names after a union
         #print(self.network)
 
-
-    def __solve_label_conflict(self, conflicted_label):
+    # does not fix labels in the network!
+    def __resolve_label(self, conflicted_label):
         # TODO: make method more efficient
         r = conflicted_label
         m = self.N[conflicted_label]
@@ -32,38 +35,75 @@ class Network:
             while m < 0:
                 r = -m
                 m = self.N[r]
-            #self.N[conflicted_label] = r  # updating causes problems, would need to update labeled network
         return r
 
-    def draw_raw_network(self, name):
-        # draw network and save it
-        plt.matshow(np.array([a[1:] for a in self.network[1:]]), cmap=cmap)
-        if self.save:
-            plt.savefig(f"out/{name}.png")
-            plt.clf()
-
-
-    def draw_labeled_network(self, name):
-        #print(self.labeled_network)
-        #print('\n')
-        #print(self.N)
-        #quantity_measured = np.sum(self.N, where=self.N > 0)
-        #quantity_set = self.network.sum() # works since True == 1
-        #print('\n'+str(quantity_measured - quantity_set))
-        fig, ax = plt.subplots()
-        ax.matshow(np.array([a[1:] for a in self.network[1:]]), cmap=cmap, interpolation='none')
-
-        for (i, j), z in np.ndenumerate(self.labeled_network):
-            if z != 0:       # don't draw empty fields
-                l = self.__solve_label_conflict(z)
+    def plot_grid(self, fig=None, ax=None, labeled=False, file_name=None, network=None):
+        if not self.analysed: self.hoshen_kopelman()
+        if ax is None:
+            if fig is None:
+                fig = plt.figure()
+            ax = fig.subplots()
+        if network is None:
+            network = np.array([a[1:] for a in self.network[1:]])
+        ax.matshow(network, cmap=cmap)
+        if labeled:
+            for i, j in np.ndindex(self.labeled_network.shape):
+                l = self.label_at(i, j)
                 ax.text(j, i, f'{l}', ha='center', va='center', color=(0.9, 0.9, 0.9))
-        if self.save:
-            plt.savefig(f'out/{name}.png')
-            plt.clf()
+            self.fixed = True
+        if file_name is not None:
+            plt.savefig(f'out/{file_name}.png')
 
+    def plot_cluster(self, cluster_label, fig=None, ax=None, labeled=False, file_name=None):
+        self.fix_labels()
+        n = np.copy(self.labeled_network) # copy to not modify this network
+        n[n != cluster_label] = 0      # set all wich are not the cluster to 0
+        n = n[~np.all(n == 0, axis=1)] # delete columns with 0
+        n = n[:, ~np.all(n == 0, axis=0)] # delete rows with 0
+        self.plot_grid(fig, ax, labeled, file_name, network=n>0)
 
+    def plot_largest_cluster(self, fig=None, ax=None, labeled=False, file_name=None):
+        self.plot_cluster(np.argmax(self.N), fig=fig, ax=ax, labeled=labeled, file_name=file_name)
+
+    def is_occupied(self, row, col):
+        return self.network[row+1][col+1]
+
+    def label_at(self, row, col):
+        l = self.labeled_network[row][col]
+        if self.fixed: return l
+        n = self.N[l]
+        if n < 0:
+            while True:
+                l = -n
+                n = self.N[l]
+                if n >= 0: break
+            self.labeled_network[row][col] = l
+        return l
+
+    def fix_labels(self):
+        if not self.analysed: self.hoshen_kopelman()
+        if self.fixed: return
+        for i, j in np.ndindex(self.labeled_network.shape):
+            self.label_at(i, j)
+        self.fixed = True
+
+    def get_perculations(self):
+        """
+        :return: an array of labels which perculate (touch top and bottom) except 0
+        """
+        if not self.analysed: self.hoshen_kopelman()
+        top = self.labeled_network[0]
+        bot = self.labeled_network[-1]
+        if not self.fixed:
+            for i in range(len(top)):
+                top[i] = self.__resolve_label(top[i])
+                bot[i] = self.__resolve_label(bot[i])
+        perc = np.intersect1d(top, bot)
+        return perc[perc != 0]
 
     def hoshen_kopelman(self):
+        if self.analysed: return
+        self.analysed = True
         max_label = int(1)                       # current max cluster label, counting up (zero shall be excluded)
         for row in range(1, self.height + 1): # first col/row is empty
 
@@ -83,13 +123,13 @@ class Network:
                         max_label += 1
                     elif tile_left != tile_above: # one neighbor is empty and one is not -> append cell
                         label = label_above if tile_above else label_left
-                        label = self.__solve_label_conflict(label)
+                        label = self.__resolve_label(label)
                         self.labeled_network[row-1][col-1] = label
                         self.N[label] += 1     
                     else: # both neighbors must be occupied -> conflict
 
-                        resolved_label_left = self.__solve_label_conflict(label_left)
-                        resolved_label_above = self.__solve_label_conflict(label_above)
+                        resolved_label_left = self.__resolve_label(label_left)
+                        resolved_label_above = self.__resolve_label(label_above)
 
                         if resolved_label_left == resolved_label_above: # left and above are the same -> append cell
                             self.labeled_network[row-1][col-1] = resolved_label_left
@@ -101,14 +141,17 @@ class Network:
 
                         #labeled_row[col-1] = resolved_label_left
                         #self.labeled_network[row-1][col] = resolved_label_above
-                        self.labeled_network[row-2][col-1] = label
-                        self.labeled_network[row-1][col-2] = label
+                        #self.labeled_network[row-2][col-1] = label
+                        #self.labeled_network[row-1][col-2] = label
                         self.labeled_network[row-1][col-1] = label
                         self.N[label] += 1
                         self.N[label] += self.N[other_label]
                         self.N[other_label] = -int(label)
+                        self.fixed = False
             # end col loop
         # end row loop                    
 
     def cluster_sizes(self):
         return dict(zip(*np.unique(np.where(self.N > 0,  self.N, np.zeros(len(self.N))), return_counts=True)))
+
+
